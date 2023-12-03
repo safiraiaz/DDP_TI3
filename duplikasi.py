f3695e744b411cd5308a3cafb81d94143192f515
safiraiaz <146071300+safiraiaz@users.noreply.github.com>
2023-12-03 12:50:30 +0700

    Add files via upload

---
+++ duplikasi.py
@@ -0,0 +1,6 @@
+daftar_buah = ["pepaya","mangga","pisang","durian","jambu"]
+hasil_duplikasi = []
+for buah in daftar_buah:
+    hasil_duplikasi +=[buah,buah]
+
+print(hasil_duplikasi)
---
+++ membalikan_nama_buah.py
@@ -0,0 +1,4 @@
+#No 2 Membalik nama buah
+daftar_buah = ["pepaya","mangga","pisang","durian","jambu"]
+
+print(daftar_buah[::-1])
